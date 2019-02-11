4fef19b6c325d5bdc3f3e624520655843eb0b652
Mark Watson <mark.g.watson@protonmail.com>
2019-02-11 13:09:55 +1000

    Config

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,7 +9,7 @@ var {Todo,User} = require('./model');
 
 var app = express();
 // Heroku
-const port = process.env.PORT || 3000
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
